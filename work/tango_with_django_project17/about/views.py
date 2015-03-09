9ec2d2216953ca6f2205854295dd43ad5cd4f2c6
2057559s <nicholassaunderson@Nicholass-MacBook-Pro.local>
2015-03-09 18:40:20 +0000

    Renamed source folder

---
+++ work/tango_with_django_project17/about/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def index(request):
+    context_dict = {'boldmessage':"This tutorial has been put together by: Nicholas Saunderson, 2057559s"}
+
+    return render(request, 'rango/about.html', context_dict)
