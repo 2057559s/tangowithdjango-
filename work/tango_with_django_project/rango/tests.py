452500f2db44a009e262e1cc2e43bbab7efd3809
2057559s <nik-saunderson@hotmail.com>
2017-01-26 10:12:46 +0000

    test

--- work/tango_with_django_project/rango/tests.py
+++ work/tango_with_django_project/rango/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import *
+from rango.forms import *
 
 # Create your tests here.
+
+
+class TestCategoryForm(TestCase):
+    
+
+    def test_Category_name(self):
+        form = CategoryForm(name="Nick", views=0)
+        self.assertEqual(self.form.name, "Nick")
+
